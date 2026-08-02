5fee350bfbd3ee4213f4a59f6020ba99d01cee3d
thewhiterabbithack <thewhiterabbit_hack@protonmail.com>
2026-08-02 20:23:06 +0200

    imagen_destacada

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     date: z.string(),
+    image: z.string().startsWith("/"),
     tags: z.array(z.string()).optional(),
   }),
 });
